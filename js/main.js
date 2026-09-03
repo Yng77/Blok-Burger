43ff29e9c598834945a4950544b0cd0b08a8c2e2
Yng77 <ev.skrypka@gmail.com>
2026-09-03 16:52:26 +0300

    mobile  adoptation - Header

--- js/main.js
+++ js/main.js
@@ -40,5 +40,16 @@
            } else {
                document.getElementById('btnUp').className = 'button-up hidden';
            }
+        }
+       
+// Menu
+
+       function openModal() {
+           document.getElementById("modal").style.top = "0px";
        }
+
+       function closeModal() {
+           document.getElementById("modal").style.top = "-400px";
+       }
+
 
